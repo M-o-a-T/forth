d23ed067ab0d0b1c5c784365afc8218405e3f713
Matthias Urlichs <matthias@urlichs.de>
2021-09-01 20:53:28 +0200

    FS: more library

--- master.fs
+++ master.fs
@@ -3,20 +3,30 @@
 compiletoflash
 
 \ declare "#if defined X" instead of "#ifdef X" to avoid Forth errors.
-\ We will need that later because the real serial handler won't tolerate any errors.
+\ We will need that later because the real serial handler won't tolerate errors.
 
 #ifndef defined
 : defined   ( "token" -- flag ) token find drop 0<> ; 
 : undefined ( "token" -- flag ) token find drop 0=  ; 
 \ delay
-: !dly #100000 0 do loop immediate ;
+\ : !dly #100000 0 do loop immediate ;
 #endif
 
 #if undefined init
 : init ;
 #endif
 
-#if undefined voc
-#include vis
+#if defined irq-systick undefined ct-irq and
+#include lib/crash.fs
 #endif
-\ #include lib/multitask.fs
+
+\ #if undefined voc
+\ #include lib/vis.fs
+\ #endif
+
+#if defined syscall undefined save and
+compiletoram
+#include lib/save.fs
+#endif
+
+#include lib/multitask.fs
