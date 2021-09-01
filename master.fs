f33430122cf5fefd9fc57f27251adfc3e90d3bdf
Matthias Urlichs <matthias@urlichs.de>
2021-09-01 12:08:54 +0200

    Make master.fs idempotent

--- master.fs
+++ master.fs
@@ -5,14 +5,18 @@ compiletoflash
 \ declare "#if defined X" instead of "#ifdef X" to avoid Forth errors.
 \ We will need that later because the real serial handler won't tolerate any errors.
 
+#ifndef defined
 : defined   ( "token" -- flag ) token find drop 0<> ; 
 : undefined ( "token" -- flag ) token find drop 0=  ; 
 \ delay
 : !dly #100000 0 do loop immediate ;
+#endif
 
 #if undefined init
 : init ;
 #endif
 
+#if undefined voc
 #include vis
+#endif
 \ #include lib/multitask.fs
