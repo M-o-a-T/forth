5467789fd1a267270210fe979784a2a886b32083
Matthias Urlichs <matthias@urlichs.de>
2021-09-13 08:34:51 +0200

    Import vis-using disasm from mecrisp

--- lib/disasm.fs
+++ lib/disasm.fs
@@ -1,9 +1,27 @@
+\    Filename: see -> vis-disassembler-m3.fs
+\     Purpose: Partial ARM Cortex M3/M4 Disassembler for Mecrisp-Stellaris
+\         MCU: ARM Cortex M3/M4
+\       Board: *
+\        Core: Mecrisp-Stellaris
+\    Required: VIS Extension vis-0.8.4-mecrisp-quintus.fs
+\ Recommended: e4thcom Terminal
+\      Author: manfred.mahlow@forth-ev.de
+\    Based on: mecrisp-stellaris/common/disassembler-m3.txt
+\     Licence: GPLv3
+\   Changelog: MM-200522  Disassembler from Mecrisp-Stellaris packed into a
+\                 VOCabulary named \disasm. voc? added, name. and see changed,
+\                 see moved to VOCabulary root, list removed.
 
-\ ARM Cortex M0 Disassembler, Copyright (C) 2013  Matthias Koch
+
+\ Partial ARM Cortex M3/M4 Disassembler, Copyright (C) 2013  Matthias Koch
 \ This is free software under GNU General Public License v3.
-\ Knows M0 machine instructions, resolves call entry points,
-\ literal pools and handles inline strings.
+\ Knows all M0 and some M3/M4 machine instructions, 
+\ resolves call entry points, literal pools and handles inline strings.
 \ Usage: Specify your target address in disasm-$ and give disasm-step some calls.
+
+FORTH DEFINITIONS  DECIMAL
+
+voc \disasm   \disasm also definitions 
 
 \ ---------------------------------------
 \  Memory pointer and instruction fetch
@@ -16,7 +34,7 @@
   2 disasm-$ +!   ;
 
 \ --------------------------------------------------
-\  Try to find address as code start in Dictionary
+\  Try to find address as code start in Dictionary 
 \ --------------------------------------------------
 
 : disasm-string ( -- ) \ Takes care of an inline string
@@ -29,11 +47,12 @@
   >r
   dictionarystart
   begin
-    dup   6 + dup skipstring r@ = if ."   --> " ctype else drop then
+\   dup 6 + dup skipstring r@ = if ."   --> " ctype else drop then
+    dup 6 + skipstring  r@ = if ."   -->  " dup inside .nid then  \ MM-200522
     dictionarynext
   until
   drop
-  r>
+  r> 
 
   case \ Check for inline strings ! They are introduced by calls to ." or s" internals.
     ['] ." $1E + of ."   -->  .' " disasm-string ." '" endof \ It is ." runtime ?
@@ -53,11 +72,11 @@
 : addr. u.8 ;
 
 : register. ( u -- )
-  case
+  case 
     13 of ."  sp" endof
     14 of ."  lr" endof
     15 of ."  pc" endof
-    dup ."  r" decimal u.ns hex
+    dup ."  r" decimal u.ns hex 
   endcase ;
 
 \ ----------------------------------------
@@ -90,6 +109,51 @@
 : imm7<<2. ( Opcode Position -- Opcode ) over swap rshift  $7F and shl shl const. ;
 : imm8<<2. ( Opcode Position -- Opcode ) over swap rshift  $FF and shl shl const. ;
 
+: condition. ( Condition -- )
+  case
+    $0 of ." eq" endof  \ Z set
+    $1 of ." ne" endof  \ Z clear
+    $2 of ." cs" endof  \ C set
+    $3 of ." cc" endof  \ C clear
+                       
+    $4 of ." mi" endof  \ N set
+    $5 of ." pl" endof  \ N clear
+    $6 of ." vs" endof  \ V set
+    $7 of ." vc" endof  \ V clear
+                       
+    $8 of ." hi" endof  \ C set Z clear
+    $9 of ." ls" endof  \ C clear or Z set
+    $A of ." ge" endof  \ N == V
+    $B of ." lt" endof  \ N != V
+                       
+    $C of ." gt" endof  \ Z==0 and N == V
+    $D of ." le" endof  \ Z==1 or N != V
+  endcase
+;
+
+: rotateleft  ( x u -- x ) 0 ?do rol loop ;
+: rotateright ( x u -- x ) 0 ?do ror loop ;
+
+: imm12. ( Opcode -- Opcode )
+  dup $FF and                 \ Bits 0-7
+  over  4 rshift $700 and or  \ Bits 8-10
+  over 15 rshift $800 and or  \ Bit  11
+  ( Opcode imm12 )
+  dup 8 rshift
+  case
+    0 of $FF and                                  const. endof \ Plain 8 Bit Constant
+    1 of $FF and                 dup 16 lshift or const. endof \ 0x00XY00XY
+    2 of $FF and        8 lshift dup 16 lshift or const. endof \ 0xXY00XY00
+    3 of $FF and dup 8 lshift or dup 16 lshift or const. endof \ 0xXYXYXYXY
+
+    \ Shifted 8-Bit Constant
+    swap
+      \ Otherwise, the 32-bit constant is rotated left until the most significant bit is bit[7]. The size of the left
+      \ rotation is encoded in bits[11:7], overwriting bit[7]. imm12 is bits[11:0] of the result.
+      dup 7 rshift swap $7F and $80 or swap rotateright const.
+  endcase
+;
+
 \ --------------------------------------
 \  Name resolving for blx r0 sequences
 \ --------------------------------------
@@ -99,6 +163,96 @@
 \ ----------------------------------
 \  Single instruction disassembler
 \ ----------------------------------
+
+: disasm-thumb-2 ( Opcode16 -- Opcode16 )
+  dup 16 lshift disasm-fetch or ( Opcode16 Opcode32 )
+
+  $F000D000 $F800D000 opcode? if  \ BL
+                                ( Opcode )
+                                ." bl  "
+                                dup $7FF and ( Opcode DestinationL )
+                                over ( Opcode DestinationL Opcode )
+                                16 rshift $7FF and ( Opcode DestinationL DestinationH )
+                                dup $400 and if $FFFFF800 or then ( Opcode DestinationL DestinationHsigned )
+                                11 lshift or ( Opcode Destination )
+                                shl 
+                                disasm-$ @ +
+                                dup addr. name. \ Try to resolve destination
+                              then
+
+  \ MOVW / MOVT
+  \ 1111 0x10 t100 xxxx 0xxx dddd xxxx xxxx
+  \ F    2    4    0    0    0    0    0
+  \ F    B    7    0    8    0    0    0
+
+  $F2400000 $FB708000 opcode? if \ MOVW / MOVT
+                                ( Opcode )
+                                dup $00800000 and if ." movt"
+                                                  else ." movw"
+                                                  then
+
+                                8 reg16. \ Destination register
+
+                                \ Extract 16 Bit constant from opcode:
+                                dup        $FF and              ( Opcode Constant* )
+                                over     $7000 and  4 rshift or ( Opcode Constant** )
+                                over $04000000 and 15 rshift or ( Opcode Constant*** )
+                                over $000F0000 and  4 rshift or ( Opcode Constant )
+                                dup ."  #" u.4
+                                ( Opcode Constant )
+                                over $00800000 and if 16 lshift destination-r0 @ or destination-r0 !
+                                                   else                             destination-r0 !
+                                                   then
+                              then
+
+  \ 
+  \ 1111 0i0x xxxs nnnn 0iii dddd iiii iiii
+  \ F    0    0    0    0    0    0    0
+  \ F    A    0    0    8    0    0    0
+
+  $F0000000 $FA008000 opcode? not if else \ Data processing, modified 12-bit immediate
+                                dup 21 rshift $F and
+                                case
+                                  %0000 of ." and" endof
+                                  %0001 of ." bic" endof
+                                  %0010 of ." orr" endof
+                                  %0011 of ." orn" endof
+                                  %0100 of ." eor" endof
+                                  %1000 of ." add" endof
+                                  %1010 of ." adc" endof
+                                  %1011 of ." sbc" endof
+                                  %1101 of ." sub" endof
+                                  %1110 of ." rsb" endof
+                                  ." ?"
+                                endcase
+                                dup 1 20 lshift and if ." s" then \ Set Flags ?
+                                8 reg16. 16 reg16. \ Destionation and Source registers
+                                imm12.
+                              then
+
+  case \ Decode remaining "singular" opcodes used in Mecrisp-Stellaris:
+
+    $EA5F0676 of ." rors r6 r6 #1" endof
+
+    $F8470D04 of ." str r0 [ r7 #-4 ]!" endof
+    $F8471D04 of ." str r1 [ r7 #-4 ]!" endof
+    $F8472D04 of ." str r2 [ r7 #-4 ]!" endof
+    $F8473D04 of ." str r3 [ r7 #-4 ]!" endof
+    $F8476D04 of ." str r6 [ r7 #-4 ]!" endof
+
+    $F8576026 of ." ldr r6 [ r7 r6 lsl #2 ]" endof
+    $F85D6C08 of ." ldr r6 [ sp #-8 ]" endof
+
+    $FAB6F686 of ." clz r6 r6" endof
+
+    $FB90F6F6 of ." sdiv r6 r0 r6" endof
+    $FBB0F6F6 of ." udiv r6 r0 r6" endof
+    $FBA00606 of ." umull r0 r6 r0 r6" endof
+    $FBA00806 of ." smull r0 r6 r0 r6" endof
+
+  endcase \ Case drops Opcode32
+  ( Opcode16 )
+;
 
 : disasm ( -- ) \ Disassembles one machine instruction and advances disasm-$
 
@@ -122,54 +276,38 @@ $D000 $F000 opcode? not if else dup $0F00 and 8 rshift       \ B(1) conditional 
                          $01 of ." bne" endof  \ Z clear
                          $02 of ." bcs" endof  \ C set
                          $03 of ." bcc" endof  \ C clear
-
+                       
                          $04 of ." bmi" endof  \ N set
                          $05 of ." bpl" endof  \ N clear
                          $06 of ." bvs" endof  \ V set
                          $07 of ." bvc" endof  \ V clear
-
+                       
                          $08 of ." bhi" endof  \ C set Z clear
                          $09 of ." bls" endof  \ C clear or Z set
                          $0A of ." bge" endof  \ N == V
                          $0B of ." blt" endof  \ N != V
-
+                       
                          $0C of ." bgt" endof  \ Z==0 and N == V
                          $0D of ." ble" endof  \ Z==1 or N != V
-
                          \ $0E: Undefined Instruction
-                         $0F of ." swi"  0 imm8.   drop exit endof
+                         \ $0F: SWI                       
                        endcase
                        space
                        dup $FF and dup $80 and if $FFFFFF00 or then
-                       shl disasm-$ @ 1 bic + 2 + addr.
+                       shl disasm-$ @ 1 bic + 2 + addr. 
                     then
 
 $E000 $F800 opcode? if ." b"                                 \ B(2) unconditional branch
                       dup $7FF and shl
                       dup $800 and if $FFFFF000 or then
-                      disasm-$ @ + 2+
+                      disasm-$ @ + 2+                     
                       space addr.
                     then
 
 $4380 $FFC0 opcode? if ." bics" 0 reg. 3 reg. then           \ BIC
 $BE00 $FF00 opcode? if ." bkpt" 0 imm8. then                 \ BKPT
 
-$F000 $F800 opcode? if disasm-fetch ( Opcode1 Opcode2 )      \ BL/BLX(1)
-                     \  dup $1000 and
-                     \  if ." bl  "
-                     \  else ." blx not available in M0"
-                     \  then
-
-                      ." bl  " ( Opcode1 Opcode2 )
-                      $7FF and ( Opcode1 DestinationL )
-                      over ( Opcode1 DestinationL Opcode1 )
-                      $7FF and ( Opcode1 DestinationL DestinationH )
-                      dup $400 and if $FFFFF800 or then ( Opcode DestinationL DestinationHsigned )
-                      11 lshift or ( Opcode1 Destination )
-                      shl
-                      disasm-$ @ +
-                      dup addr. name. \ Try to resolve destination
-                    then
+\ BL/BLX handled as Thumb-2 instruction on M3/M4.
 
 $4780 $FF87 opcode? if ." blx"  3 reg16. then                \ BLX(2)
 $4700 $FF87 opcode? if ." bx"   3 reg16. then                \ BX
@@ -239,11 +377,50 @@ $3800 $F800 opcode? if ." subs" 8 reg. 0 imm8. then          \ SUB(2)
 $1A00 $FE00 opcode? if ." subs" 0 reg. 3 reg. 6 reg. then    \ SUB(3)
 $B080 $FF80 opcode? if ." sub sp" 0 imm7<<2. then            \ SUB(4)
 
+$DF00 $FF00 opcode? if ." swi"  0 imm8. then                 \ SWI
 $B240 $FFC0 opcode? if ." sxtb" 0 reg. 3 reg. then           \ SXTB
 $B200 $FFC0 opcode? if ." sxth" 0 reg. 3 reg. then           \ SXTH
 $4200 $FFC0 opcode? if ." tst"  0 reg. 3 reg. then           \ TST
 $B2C0 $FFC0 opcode? if ." uxtb" 0 reg. 3 reg. then           \ UXTB
 $B280 $FFC0 opcode? if ." uxth" 0 reg. 3 reg. then           \ UXTH
+
+
+\ 16 Bit Thumb-2 instruction ?
+
+$BF00 $FF00 opcode? not if else                              \ IT...
+                      dup $000F and
+                      case
+                        $8 of ." it" endof
+
+                        over $10 and if else $8 xor then
+                        $C of ." itt" endof
+                        $4 of ." ite" endof
+
+                        over $10 and if else $4 xor then
+                        $E of ." ittt" endof
+                        $6 of ." itet" endof
+                        $A of ." itte" endof
+                        $2 of ." itee" endof
+
+                        over $10 and if else $2 xor then
+                        $F of ." itttt" endof
+                        $7 of ." itett" endof
+                        $B of ." ittet" endof
+                        $3 of ." iteet" endof
+                        $D of ." ittte" endof
+                        $5 of ." itete" endof
+                        $9 of ." ittee" endof
+                        $1 of ." iteee" endof
+                      endcase
+                      space
+                      dup $00F0 and 4 rshift condition.
+                    then
+
+\ 32 Bit Thumb-2 instruction ?
+
+$E800 $F800 opcode? if disasm-thumb-2 then
+$F000 $F000 opcode? if disasm-thumb-2 then
+
 
 \ If nothing of the above hits: Invalid Instruction... They are not checked for.
 
@@ -282,6 +459,8 @@ drop \ Forget opcode
 \  Disassembler for definitions
 \ ------------------------------
 
+  ROOT DEFINITIONS
+
 : seec ( -- ) \ Continues to see
   base @ hex cr
 
@@ -295,7 +474,11 @@ drop \ Forget opcode
   base !
 ;
 
+sticky
 : see ( -- ) \ Takes name of definition and shows its contents from beginning to first ret
   ' disasm-$ !
   seec
 ;
+
+FORTH ONLY DEFINITIONS
+
