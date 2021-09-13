15db8b00e18b1d2ace43c8901218848c959dfa55
Matthias Urlichs <matthias@urlichs.de>
2021-09-13 15:43:47 +0200

    Move introspection

--- lib/multitask.fs
+++ lib/multitask.fs
@@ -213,37 +213,6 @@ boot-task variable last-task
   drop 0
 ;
 
-: .task
-    dup ." Task:" .word-off
-    dup 1 cells + @ ." State: " .
-    dup boot-task <> if
-    dup task-sp find-beef ." S:" .
-    dup task-rp find-beef ." R:" .
-    then
-    dup 3 cells + @ ?dup if ." Err:" .word then
-    dup 4 cells + @ ?dup if ." Check:" .word then
-    cr drop
-;
-
-: tasks ( -- ) \ Show tasks currently in round-robin list
-  \ The list may change, so first take a snapshot
-  base @ hex
-  eint? dint >r
-  0
-  up @ begin ?dup while dup @ repeat
-  irq-task @ begin ?dup while dup @ repeat
-  r> if eint then
-
-  cr begin
-    ( 0 tasks… )
-    ?dup while
-    .task
-    ( 0 one-fewer-tasks… )
-  repeat
-
-  base !
-;
-
 \ --------------------------------------------------
 \  Exception handling
 \ --------------------------------------------------
