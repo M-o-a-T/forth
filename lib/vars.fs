0fb901586a301e2d313c92dad945cd463c44a2a6
Matthias Urlichs <matthias@urlichs.de>
2021-09-14 20:14:55 +0200

    ADD lib/vars

---
+++ lib/vars.fs
@@ -0,0 +1,42 @@
+voc: var>
+
+class: int
+
+__ivar
+  cell+
+__seal
+
+: @ ( a -- n ) @ inline ;
+: ! ( n a -- ) ! inline ;
+: ? ( a -- )  @ . ;
+
+
+forth definitions only
+
+
+class: cint
+
+__ivar
+  1+
+__seal
+
+: @ ( a -- n ) c@ inline ;
+: ! ( n a -- ) c! inline ;
+: ? ( a -- )  c@ base @ swap hex . base ! ;
+
+
+forth definitions only
+
+class: hint
+
+__ivar
+  2 +
+__seal
+
+: @ ( a -- n ) h@ inline ;
+: ! ( n a -- ) h! inline ;
+: ? ( a -- )  h@ ! ;
+
+
+forth definitions only
+
