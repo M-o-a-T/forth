e72ce8375836a347a6252a8ddbbdb17764919f98
Matthias Urlichs <matthias@urlichs.de>
2021-09-14 20:15:10 +0200

    Add test for classes

---
+++ test/class.fs
@@ -0,0 +1,62 @@
+#if undefined var>
+#include lib/vars.fs
+#endif
+
+forth definitions
+
+.s
+
+var> also
+
+.s
+
+int object: i1
+
+.s
+
+12345 i1 !
+i1 ?
+.s
+i1 _addr_  dup hex.  @ .
+.s
+
+int class: uint
+.s
+: show ( a-addr -- ) __ @ u. ;
+.s
+
+__ ?? .s
+
+__ ?? ..
+
+
+class-root class: point
+__ivar
+  int ivar: x
+  int ivar: y
+__seal
+
+
+point object: p1
+
+#100 p1 x !  #200 p1 y !
+
+p1 x ?
+p1 y ?
+
+: get ( a-addr -- x y ) dup __ y @  swap __ x @ ;
+
+: set ( x y a-addr -- ) dup >r __ y ! r> __ x ! ;
+
+: show ( a-addr -- )    dup __ x ? __ y ? ;
+
+  p1 show
+
+  #1 #2 p1 set
+
+  p1 show
+
+  p1 get .s 
+
+  2drop  .s
+
