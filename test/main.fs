f6badab7eba06363df561a5522303474b87e91ec
Matthias Urlichs <matthias@urlichs.de>
2021-09-17 16:01:55 +0200

    re-sort imports
    also forget stuff

--- test/main.fs
+++ test/main.fs
@@ -1,18 +1,52 @@
 #include test/init.fs
 
+compiletoflash
+
 #if undefined class:
 #include lib/class.fs
 #endif
 
+
 #if undefined int
+compiletoram
 #include test/class.fs
+forgetram
+#endif
+
+
+\ Testing multicast on real hardware requires IRQ-based input
+\ which requires ring buffers, but ring buffers require abort
+\ which is modified when multitask gets loaded.
+\
+\ Thus this order.
+
+#if-flag multi
+#if undefined \multi
+compiletoflash
+#include lib/multicast.fs
+#endif
 #endif
 
 #if undefined ring
+compiletoflash
 #include lib/ring.fs
 #endif
 
+compiletoram
 #if undefined rr
 #include test/ring.fs
+forgetram
 #endif
+
+compiletoram
+#if undefined tasks
+#include debug/multicast.fs
+#endif
+
+#if-flag multi
+#if undefined timetask
+#include test/multicast.fs
+forgetram
+#endif
+
 
