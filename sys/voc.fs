384ca63839efc6693ff218cff8dc59fb497f965b
Matthias Urlichs <matthias@urlichs.de>
2021-09-25 21:10:42 +0200

    Fix VOCNEXT
    The previous test for naked word lists was wrong, but this one isn't.

--- sys/voc.fs
+++ sys/voc.fs
@@ -387,7 +387,7 @@ get-order \voc-wl swap 1+ set-order
 \ Given a vocs wid return the wid of the parent voc or zero if no voc was
 \ inherited.
 : vocnext ( wid1 -- wid2|0 )
-  2 cells - @ tag>wid ;
+  dup wid? if dup - else 2 cells - @ tag>wid then ;
 
 
 \ Search the VOCs search order (voc-context) at a-addr for a match with the
