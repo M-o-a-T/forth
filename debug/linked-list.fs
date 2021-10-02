b8d1d2c3a3fe362c982680c768e70a157d763448
Matthias Urlichs <matthias@urlichs.de>
2021-10-02 17:40:13 +0200

    linked-list debug: don't print self
    that should be done by the caller

--- debug/linked-list.fs
+++ debug/linked-list.fs
@@ -4,7 +4,7 @@
 \voc \d-list definitions also
 
 : ? ( list -- )
-  ." DL:" dup dup hex. .word-off
+  ." DL:" \ dup dup hex. .word-off
   dup
   ." <" __ prev @ dup hex. .word-off
   ." >" __ next @ dup hex. .word-off
