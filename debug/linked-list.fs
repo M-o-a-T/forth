836043452356d443fb433107c38d8452db0b5556
Matthias Urlichs <matthias@urlichs.de>
2021-10-04 18:33:08 +0200

    Clean up linked list debug
    Don't print links twice, or at all if empty
    When debugging, recognize poisoned links

--- debug/linked-list.fs
+++ debug/linked-list.fs
@@ -5,10 +5,25 @@
 
 : ? ( list -- )
   ." DL:" \ dup dup hex. .word-off
-  dup
-  ." <" __ prev @ dup hex. .word-off
-  ." >" __ next @ dup hex. .word-off
-  cr
+  dup ( list list )
+  2dup __ next @ .. = if
+    ." same" 2drop exit
+  then
+  __ next @ swap __ prev @
+  ( next prev )
+#if-flag debug
+  over poisoned = over poisoned = or if
+    2drop ." unlinked"
+    exit
+  then
+#endif
+  dup ." <" dup hex. .word-off
+  ." >"
+  over = if
+    ." same" drop
+  else
+    dup hex. .word-off
+  then
 ;
 
 previous
