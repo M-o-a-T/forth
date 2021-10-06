c8b700ce80d93be5fc44c85a828499f93065facd
Matthias Urlichs <matthias@urlichs.de>
2021-10-06 03:48:55 +0200

    forgot to check in timeout test

---
+++ test/timeout.fs
@@ -0,0 +1,57 @@
+#if-flag !multi
+#end
+#endif
+
+#if undefined time
+#include lib/timeout.fs
+#endif
+
+:task: t1
+  ." T1 !" cr
+  10000 task sleep
+  ." T1 A" cr
+  20000 task sleep
+  ." T1 B" cr
+  30000 task sleep
+  ." T1 C" cr
+  40000 task sleep
+  ." T1 D" cr
+;
+
+:task: t2
+  ." T2 !" cr
+  30000 task sleep
+  ." T2 A" cr
+  20000 task sleep
+  ." T2 B" cr
+  10000 task sleep
+  ." T2 C" cr
+  40000 task sleep
+  ." T2 D" cr
+;
+
+:task: t3
+  ." T3 !" cr
+  5000 task sleep
+  ." T3 A" cr
+  55000 task sleep
+  ." T3 B" cr
+  100000 task sleep
+  ." T3 C" cr
+  1000 task sleep
+  ." T3 D" cr
+;
+
+time first
+
+\ now let's do some real tests
+
+t1 start  t2 start  t3 start
+
+task also
+: yy
+  begin t1 state @ =dead <> while yield repeat
+  begin t2 state @ =dead <> while yield repeat
+  begin t3 state @ =dead <> while yield repeat
+;
+yy
