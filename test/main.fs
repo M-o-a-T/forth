7f775ff9fc930d9d0d27f35a4376dbde08269f52
Matthias Urlichs <matthias@urlichs.de>
2021-10-18 21:21:41 +0200

    Streamline testing

--- test/main.fs
+++ test/main.fs
@@ -1,68 +1,13 @@
 #include test/init.fs
 
-compiletoflash
-
-#if-flag debug
-compiletoflash
-
-#if undefined ??
-#include debug/voc.fs
-#endif
-#endif
-
-compiletoflash
-
-#if undefined class:
-#include lib/class.fs
-#endif
-
-compiletoflash
-
-#if undefined var>
-#include lib/vars.fs
-#endif
-
-
-#if undefined p1
+#include test/bits.fs
+#include test/mach-id.fs
 #include test/offset.fs
-#endif
-
-test-drop
-test-ram
-
-#if undefined .all
-#include debug/class.fs
-#endif
-
-#if undefined yy
 #include test/timeout.fs
-#endif
-
-test-drop
-test-ram
-
-#if undefined p1
 #include test/class.fs
-#endif
-
-test-ram
-#if undefined .all
-#include debug/class.fs
-#endif
-\cls (' root-cls .all
-
-test-drop
-
-compiletoflash
-
-#require d-list-head lib/linked-list.fs
-#require th test/linked-list.fs
-
-\ Testing multicast on real hardware requires IRQ-based input
-\ which requires ring buffers, but ring buffers require abort
-\ which is modified when multitask gets loaded.
-\
-\ Thus this order.
+#include test/linked-list.fs
+#include test/io.fs
+#include test/syscall.fs
 
 #if-flag multi
 #if undefined \multi
@@ -72,63 +17,16 @@ compiletoflash
 #endif
 \ if-flag multi
 
-compiletoflash
-
-#if undefined ring
-#include lib/ring.fs
-#endif
-
-test-ram
-
-#if undefined rr
+\ Testing multitasking on real hardware requires IRQ-based input
+\ which requires ring buffers, but ring buffers require abort
+\ which is modified when multitask gets loaded.
+#include test/abort.fs
 #include test/ring.fs
-#endif
+#include test/timeout.fs
 
-#if-flag multi
-
-test-drop
-
-compiletoflash
-
-#if undefined term
-#include lib/mt-term.fs
-test-ram
 #include test/mt-term.fs
-#endif
-
-test-ram
-
-#if-flag debug
-#if undefined tasks
-#include debug/multitask.fs
-#endif
-#endif
-
-test-ram
-
-#if undefined timetask
 #include test/multitask.fs
-#endif
-
-#endif
 \ if-flag multi
-
-test-drop
-
-#if defined syscall
-
-#include lib/syscall.fs
-#include test/syscall.fs
-
-#endif
-
-#if undefined BFT
-#include test/bits.fs
-#endif
-
-#include test/mach-id.fs
-
-test-drop
 
 \ SPDX-License-Identifier: GPL-3.0-only
 #ok depth 0=
---
+++ test/reset.fs
@@ -0,0 +1,11 @@
+\ This resets the system.
+
+#delay 5
+#-ok .. forth reset
+#delay 0.5
+#ok 1
+
+compiletoram
+
+\ SPDX-License-Identifier: GPL-3.0-only
+#ok depth 0=
