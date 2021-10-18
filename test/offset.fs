61e46715b7e1f3624f6c70ec3aa844b6c0828736
Matthias Urlichs <matthias@urlichs.de>
2021-10-18 21:25:06 +0200

    drop inclusion test
    not sensible here

--- test/offset.fs
+++ test/offset.fs
@@ -4,7 +4,6 @@ compiletoflash
 #require offset: lib/util.fs
 compiletoram
 
-#if undefined gpio
 voc: gpio
 $00 offset: in ( a1 -- a2 )
 $02 offset: out
@@ -13,10 +12,6 @@ $04 offset: dir
 : port: ( "name" a -- ) item constant ;
 $1230 gpio port: p1 ( -- a1 )
 $2340 gpio port: p2 ( -- a1 )
-
-#else
-gpio also
-#endif
 
 #ok p1 out $1232 =
 #ok p2 dir $2344 =
