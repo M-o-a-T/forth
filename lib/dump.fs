cc0c46e8dcce741ee4123b0fc8b94cbde13ff8ca
Matthias Urlichs <matthias@urlichs.de>
2021-11-03 16:10:48 +0100

    Add dump code for byte-based output

--- lib/dump.fs
+++ lib/dump.fs
@@ -48,6 +48,43 @@ forth only definitions
   2drop
 ;
 
+: dump16.8 ( addr -- ) \ Print 16 bytes memory
+  base @ hex swap  ( base addr )
+  dup $F bic swap $F and  ( base addr< addrf )
+  over hex. ." :"
+
+  over dup 16 + swap do ( base addr< addrf )
+    i $F and ( base addr f if )
+    space dup 3 and 0= if space then
+    over < if 2 spaces else
+    i @ u.2
+    then
+  loop
+  drop
+
+  ."  | "
+
+  dup 16 + swap do
+        i c@ 32 u>= i c@ 127 u< and if i c@ emit else [char] . emit then
+        i $F and 7 = if 2 spaces then
+      loop
+
+  ."  |" cr
+  base !
+;
+
+: dump8 ( addr len -- ) \ Print a memory region
+  cr
+  over + swap ( end addr )
+  begin
+    dup dump16.8
+    $f bic
+    $10 + ( end addr+16 )
+    2dup <=
+  until
+  2drop
+;
+
 : list ( -- )
   cr
   dictionarystart
